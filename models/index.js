63e6ac72bed70c8c838c61b6dfae549ce3370598
markpcomer <markpcomer@gmail.com>
2024-04-23 18:11:12 -0400

    updated models/index

--- models/index.js
+++ models/index.js
@@ -18,12 +18,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'ProductTag',
+  as: 'productTag_products',
   foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'ProductTag',
+  as: 'productTag_products',
   foreignKey: 'tag_id',
 });
 
